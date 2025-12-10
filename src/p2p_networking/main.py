2cbfe2fbc11141e24713bcc604b43fded0f4006e
Seki Rin <NikolayNikolaevichBlnv@gmail.com>
2025-12-10 17:49:43 +0300

    Fix bugs in main.py

--- src/p2p_networking/main.py
+++ src/p2p_networking/main.py
@@ -122,4 +122,4 @@ async def get_index():
         return f.read()
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
+    uvicorn.run('p2p_networking.main:app', host='127.0.0.1', port=8000, reload=True)
